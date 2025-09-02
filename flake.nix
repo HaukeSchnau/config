4f4360f060d592d81d0f3fcb5d3749ca41a4ca96
Hauke Schnau <hauke@schnau.dev>
2025-09-02 13:40:08 +0200

    Intial commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    # Where we get most of our software. Giant mono repo with recipes
+    # called derivations that say how to build software.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Manages configs links things into your home directory
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./hosts/default/configuration.nix
+        inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
